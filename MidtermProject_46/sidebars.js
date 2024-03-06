dec1602363594ce9e66baa5ea4758b7db656c03d
em-bd <emmieb@iastate.edu>
2024-03-06 17:00:05 -0600

    made a price bar filter and set up a checkbox

--- MidtermProject_46/sidebars.js
+++ MidtermProject_46/sidebars.js
@@ -1,11 +1,26 @@
 /**
  * 
  */
+
+// side bar collapsable
 (() => {
-    'use strict'
-    const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    tooltipTriggerList.forEach(tooltipTriggerEl => {
-      new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-  })()
-  
+  'use strict'
+  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(tooltipTriggerEl => {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
+
+
+// price range slider
+const sliderEl = document.getElementById("pricerangeslider");
+const sliderValue = document.querySelector(".pricevalue");
+
+sliderEl.addEventListener("input", (event) => {
+  const tempSliderValue = event.target.value;
+  sliderValue.textContent = tempSliderValue;
+
+  const progress = (tempSliderValue / sliderEl.max) * 100;
+
+  sliderEl.style.background = `linear-gradient(to right, #04AA6D ${progress}%, #d3d3d3 ${progress}%)`;
+})
