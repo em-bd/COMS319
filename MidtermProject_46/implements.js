5fd357299c2cdbd4783a8ae28b4b340c62dee6d3
Sam-Craft1 <sam@thecrafts.net>
2024-03-07 17:52:07 -0600

    Pages pushing data and images from JSON

--- MidtermProject_46/implements.js
+++ MidtermProject_46/implements.js
@@ -8,6 +8,34 @@
 
 function loadImplements(myProducts)   {
     // load implements
+
+    var productTab = document.getElementById("col")
+
+    productTab.innerHTML = ""
+
+    for (let i = 0; i < myProducts.implements.length; i++) {
+        
+        let source = myProducts.implements[i].src;
+        let name = myProducts.implements[i].name;
+
+        let addImplementTab = document.createElement("div")
+
+        addImplementTab.classList.add("col")
+
+        addImplementTab.innerHTML = `
+
+
+        <div class="card text-bg-dark">
+           <img src = ${source} class = "card-img-top" alt = "..."></img>
+            <div class="card-body">
+              <p class="card-text"><strong>${name}</strong></p>
+              <div class="d-flex justify-content-between align-items-center">
+              </div>
+            </div>
+          </div>
+          `
+        productTab.appendChild(addImplementTab)
+    }
 }
 
 fetch("./product.json")
--- MidtermProject_46/vehicles.js
+++ MidtermProject_46/vehicles.js
@@ -8,8 +8,36 @@
 
 function loadVehicles(myProducts)   {
     //TODO: load vehicles
+
+    var vehicleTab = document.getElementById("col")
+
+    vehicleTab.innerHTML = ""
+
+    for (let i = 0; i < myProducts.vehicles.length; i++) {
+        
+        let source = myProducts.vehicles[i].src;
+        let name = myProducts.vehicles[i].name;
+
+        let addvehicleTab = document.createElement("div")
+
+        addvehicleTab.classList.add("col")
+
+        addvehicleTab.innerHTML = `
+
+
+        <div class="card text-bg-dark">
+           <img src = ${source} class = "card-img-top" alt = "..."></img>
+            <div class="card-body">
+              <p class="card-text"><strong>${name}</strong></p>
+              <div class="d-flex justify-content-between align-items-center">
+              </div>
+            </div>
+          </div>
+          `
+        vehicleTab.appendChild(addvehicleTab)
+    }
 }
 
-fetch("./products.json")
+fetch("./product.json")
     .then(response => response.json())
     .then(myProducts => loadVehicles(myProducts));
