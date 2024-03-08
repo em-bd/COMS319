35174a898f65656a3b3a8f1db7ae641b75db6fb8
em-bd <emmieb@iastate.edu>
2024-03-07 18:10:04 -0600

    some sidebar work

--- MidtermProject_46/display.js
+++ MidtermProject_46/display.js
@@ -3,11 +3,14 @@
  */
 
 function loadProducts(myProducts) {
+    let product = localStorage.getItem('name');
+    let type = localStorage.getItem('type');
     var maincontainer = document.getElementById("product");
     // do something
 }
 
 // find a way to pass/fetch which was the previous page and what product was selected
+
 
 fetch("./products.json")
 .then(response => response.json())
--- MidtermProject_46/sidebars.js
+++ MidtermProject_46/sidebars.js
@@ -13,8 +13,8 @@
 
 
 // price range slider
-const sliderEl = document.getElementById("pricerangeslider");
-const sliderValue = document.querySelector(".pricevalue");
+const sliderEl = document.getElementById("minpricerangeslider");
+const sliderValue = document.querySelector(".minpricevalue");
 
 sliderEl.addEventListener("input", (event) => {
   const tempSliderValue = event.target.value;
@@ -23,4 +23,16 @@ sliderEl.addEventListener("input", (event) => {
   const progress = (tempSliderValue / sliderEl.max) * 100;
 
   sliderEl.style.background = `linear-gradient(to right, #04AA6D ${progress}%, #d3d3d3 ${progress}%)`;
-})
+});
+
+const sliderEl1 = document.getElementById("maxpricerangeslider");
+const sliderValue1 = document.querySelector(".maxpricevalue");
+
+sliderEl1.addEventListener("input", (event1) => {
+  const tempSliderValue1 = event1.target.value;
+  sliderValue1.textContent = tempSliderValue1;
+
+  const progress1 = (tempSliderValue1 / sliderEl1.max) * 100;
+
+  sliderEl1.style.background = `linear-gradient(to right, #d3d3d3 ${progress1}%, #04AA6D ${progress1}%)`;
+});
