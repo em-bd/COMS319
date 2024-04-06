4d9e79dba44177fe1f12f29984d36c3f3d1b3556
em-bd <emmieb@iastate.edu>
2024-04-05 19:07:36 -0500

    fixed

--- 46_assignment02/src/App.js
+++ 46_assignment02/src/App.js
@@ -15,40 +15,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import items from "./products.json";
 
 
-    // product rendering:
-    const render_products = (ProductsCategory) => {
-      return (<div className="category-section fixed">    
-        <div className="m-6 p-3 mt-10 m1-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 x1:gap-x-10" style={{ maxHeight: '800px', overflowY: 'scroll'}}>
-          {ProductsCategory.map((product, index) => (
-            <div key={index} className="group relative shadow-lg">
-              <div className="min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
-                <img
-                  alt="Product Image"
-                  src={product.image}
-                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                />
-              </div>
-              <div className="flex justify-between p-3">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0"/>
-                      <span style={{ fontSize: '16px', fontWeight: '600' }}>{product.title}</span>
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate}</p>
-                  <button type="button" variant="light" onClick={() => removeFromCart(product)} > - </button>{" "}
-                  <button type="button" variant="light" onClick={() => addToCart(product)}> + </button>
-                </div>
-                <p className="text-sm font-medium text-green-600">${product.price}</p>
-              </div>
-            </div>
-          ))}
-          </div>     
-      </div>);
-    }
-
-
 // app rendering:
 function App() {
   // browse:
@@ -86,6 +52,39 @@ function App() {
   // browse items:
   function Browse() {
 
+        // product rendering:
+        const render_products = (ProductsCategory) => {
+          return (<div className="category-section fixed">    
+            <div className="m-6 p-3 mt-10 m1-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 x1:gap-x-10" style={{ maxHeight: '800px', overflowY: 'scroll'}}>
+              {ProductsCategory.map((product, index) => (
+                <div key={index} className="group relative shadow-lg">
+                  <div className="min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
+                    <img
+                      alt="Product Image"
+                      src={product.image}
+                      className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                    />
+                  </div>
+                  <div className="flex justify-between p-3">
+                    <div>
+                      <h3 className="text-sm text-gray-700">
+                        <a href={product.href}>
+                          <span aria-hidden="true" className="absolute inset-0"/>
+                          <span style={{ fontSize: '16px', fontWeight: '600' }}>{product.title}</span>
+                        </a>
+                      </h3>
+                      <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate}</p>
+                      <button type="button" variant="light" onClick={() => removeFromCart(product)} > - </button>{" "}
+                      <button type="button" variant="light" onClick={() => addToCart(product)}> + </button>
+                    </div>
+                    <p className="text-sm font-medium text-green-600">${product.price}</p>
+                  </div>
+                </div>
+              ))}
+              </div>     
+          </div>);
+        }
+
     // update react hooks:
     const updateHooks = () => {
       setViewer(1); // go to cart page
@@ -110,8 +109,8 @@ function App() {
               focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700
               dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
               dark:focus:ring-blue-500 dark:focus:border-blue-500" type="search" value={query} placeholder="Search" aria-label="Search" onChange={handleChange}/>
-            <button type="button" class="btn btn-primary justify-content-end align-middle" onClick={updateHooks}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart float-left align-middle" viewBox="0 0 16 16">
+            <button type="button" class="btn btn-primary justify-content-end" onClick={updateHooks}>
+                <svg xmlns="http://www.w3.org/2000/svg"  width="16" height="16" fill="currentColor" class="bi bi-cart float-left align-middle">
                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"></path>
                 </svg>&nbsp;Cart </button>
         </div>
