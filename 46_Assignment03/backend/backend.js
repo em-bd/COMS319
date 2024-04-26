17e30138b9b324d50857ce58cae573b2481099b0
em-bd <emmieb@iastate.edu>
2024-04-26 10:49:27 -0500

    backend stuff added

--- 46_Assignment03/backend/backend.js
+++ 46_Assignment03/backend/backend.js
@@ -24,7 +24,7 @@ app.listen(port, () => {
 
 // MongoDB
 const url = "mongodb://127.0.0.1:27017";
-const dbName = "secoms319";
+const dbName = "reactdata";
 const client = new MongoClient(url);
 const db = client.db(dbName);
 
@@ -34,14 +34,49 @@ const db = client.db(dbName);
  * Mongodb catalogue
  */
 app.get("/products", async (req, res) => {
+    await client.connect();
+    console.log("Node connected successfully to GET MongoDB");
 
+    const query = {};
+
+    const results = await db
+        .collection("fakestore_catalog")
+        .find(query)
+        .limit(100)
+        .toArray();
+
+    console.log(results);
+    res.status(200);
+    res.send(results);
+});
+
+/**
+ * GET id request:
+ * Reads product with that specific id
+ */
+app.get("/products/:id", async (req, res) => {
+    const productID = Number(req.params.id);
+    console.log("Product to find :", productID);
+
+    await client.connect();
+    console.log("Node connected successfully to GET-id MongoDB");
+    const query = { "id" : productID };
+    const results = await db
+        .collection("fakestore_catalog")
+        .findOne(query);
+
+    console.log("Results :", results);
+    if (!results)
+        res.send("Not found").status(404);
+    else
+        res.send(results).status(200);
 });
 
 /**
  * PUT request:
  * Updates the price of an object
  */
-app.put("/products/:id", async (req, res) => {
+app.put("/updateProduct/:id", async (req, res) => {
     try {
         const id = Number(req.params.id);
         const query = { id : id };
@@ -59,14 +94,61 @@ app.put("/products/:id", async (req, res) => {
 
     } catch (error) {
         console.error("Error updating product price :", error);
-        resizeTo.status(500).send({message : 'Internal Server Error'});
+        res.status(500).send({message : 'Internal Server Error'});
     }
 });
 
 /**
- * 
+ * POST request:
+ * Will add a new product
  */
+app.post("/addProduct", async (req, res) => {
+    try {
+        await client.connect();
+        const values = Object.values(req.body);
 
+        const newDocument = {
+            "id" : values[0],
+            "title" : values[1],
+            "price" : values[2],
+            "description" : values[3],
+            "category" : values[4],
+            "image" : values[5],
+            "rating" : values[6]
+        };
 
-// DELETE request:
+        console.log(newDocument);
 
+        const results = await db
+            .collection("fakestore_catalog")
+            .insertOne(newDocument);
+        res.status(200);
+        res.send(results);
+    } catch (error) {
+        console.error();
+        res.status(500).send({message : 'Internal Server Error'});
+    }
+});
+
+/**
+ * DELETE request:
+ * Deletes Product with the specified id
+ */
+app.delete("/deleteProduct/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+
+        await client.connect();
+        console.log("Product to delete :", id);
+
+        const query = { id : id };
+
+        // delete
+        const results = await db.collection("fakestore_catalog").deleteOne(query);
+        res.status(200);
+        res.send(results);
+    } catch (error) {
+        console.error("Error deleting product :", error);
+        res.status(500).send({ message : 'Internal Server Error' });
+    }
+});
