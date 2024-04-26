0cd3a32d8f5d20e1c1698ff419fc8f520e34a624
Sam-Craft1 <sam@thecrafts.net>
2024-04-26 11:21:35 -0500

    Frontend go brrrr

--- 46_Assignment03/frontend/src/App.js
+++ 46_Assignment03/frontend/src/App.js
@@ -6,33 +6,33 @@
  * Date: April 27th, 2024
  */
 
-import logo from "./logo.svg";
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
-const [product, setProduct] = useState([]);
-const [oneProduct, setOneProduct] = useState([]);
-// new Product
-const [addNewProduct, setAddNewProduct] = useState({
-  id: 0,
-  title: "",
-  price: 0.0,
-  description: "",
-  category: "",
-  image: "",
-  rating: 0.0,
-});
-
-const [viewer, setViewer] = useState(0);
-
 function App() {
+  const [product, setProduct] = useState([]);
+  const [oneProduct, setOneProduct] = useState([]);
+  // new Product
+  const [addNewProduct, setAddNewProduct] = useState({
+    id: 0,
+    title: "",
+    price: 0.0,
+    description: "",
+    category: "",
+    image: "",
+    rating: 0.0,
+  });
+
+  const [viewer, setViewer] = useState(0);
+
   useEffect(() => {
     getAllProducts();
   }, []);
   function getAllProducts() {
-    fetch("localhost:8081/products")
+    fetch("http://localhost:8081/products")
       .then((response) => response.json())
       .then((data) => {
         console.log("Show Catalog of Products :");
@@ -48,21 +48,22 @@ function App() {
       Title: {el.title} <br />
       Category: {el.category} <br />
       Price: {el.price} <br />
-      Rating :{el.rating} <br />
+      Rating :{el.rating[0]} <br />
+      Count :{el.rating[1]} <br />
     </div>
   ));
 
   function getOneProduct(id) {
     console.log(id);
     if (id >= 1 && id <= 20) {
-      fetch("localhost:8081/products" + id)
+      fetch("http://localhost:8081/products" + id)
         .then((response) => response.json())
         .then((data) => {
           console.log("Show one product :", id);
           console.log(data);
           setOneProduct(data);
         });
-       setViewer(1);
+      setViewer(1);
     } else {
       console.log("Wrong number of Product id.");
     }
@@ -74,7 +75,8 @@ function App() {
       Title: {el.title} <br />
       Category: {el.category} <br />
       Price: {el.price} <br />
-      Rating: {el.rating} <br />
+      Rating: {el.rating[0]} <br />
+      Count: {el.rating[1]} <br />
     </div>
   ));
 
@@ -84,7 +86,7 @@ function App() {
 
       <h3>Show all available Products.</h3>
       <button onClick={() => getAllProducts()}>Show All ...</button>
-      {(viewer === 0) && showAllItems}
+      {viewer === 0 && showAllItems}
 
       <h3>Show one Product by Id:</h3>
       <input
@@ -94,12 +96,7 @@ function App() {
         placeholder="id"
         onChange={(e) => getOneProduct(e.target.value)}
       />
-      {(viewer === 1) && showOneItem}
-
-      {viewer === 0 && <Create />}
-      {viewer === 1 && <Remove />}
-      {viewer === 2 && <Update />}
-      {viewer === 3 && <Delete />}
+      {viewer === 1 && showOneItem}
     </div>
   );
 }
