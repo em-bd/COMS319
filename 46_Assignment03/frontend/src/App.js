e17af467b396c38bc25799162e8367037a2a138d
Sam-Craft1 <sam@thecrafts.net>
2024-04-26 10:19:46 -0500

    Merging?

--- 46_Assignment03/frontend/src/App.js
+++ 46_Assignment03/frontend/src/App.js
@@ -2,22 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(viewer === 0) && <Create />}
+      {(viewer === 1) && <Remove />}
+      {(viewer === 2) && <Update />}
+      {(viewer === 3) && <Delete />}
     </div>
   );
 }
