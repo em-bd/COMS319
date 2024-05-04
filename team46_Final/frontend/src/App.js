3e4b9edeead7c77d1ecadf7a20346e0f9db891d0
em-bd <emmieb@iastate.edu>
2024-05-04 13:41:47 -0500

    stuff

--- team46_Final/frontend/src/App.js
+++ team46_Final/frontend/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Authors: Em Bradley-DeHaan,
+ *          Samuel Craft
+ * ISU Netids: emmieb@iastate.edu
+ *          craftsam@iastate.edu
+ * Date: May 9th, 2024
+ */
+
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "Main";
+import Implements from "Implements";
+import Vehicles from "Vehicles";
 
 function App() {
+  // react hooks viewer:
+  const [viewer, setViewer] = useState(0);
+
+  function updateHooks(i) {
+    setViewer(i);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {viewer == 0 && <Main />}
+      {viewer == 1 && <Products />}
+      {viewer == 2 && <Vehicles />}
+      {viewer == 3 && <Implements />}
+      {viewer == 4 && <Cart />}
+      {viewer == 5 && <Summary />}
+      {viewer == 6 && <AboutUs />}
     </div>
   );
 }
